4ad2a65e2a4e73cf799c67b8bff79aeeb0bdac6f
Derek Woo <me@derekw.co>
2023-11-22 14:39:28 -0800

    Add MainApplication.java

---
+++ src/main/java/ca/bcit/comp2522/termproject/pix/MainApplication.java
@@ -0,0 +1,50 @@
+package ca.bcit.comp2522.termproject.pix;
+
+import ca.bcit.comp2522.termproject.pix.gamecontroller.GameController;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * Represents the main application window.
+ *
+ * @author Nathan Yau
+ * @author Derek Woo
+ * @version 2023
+ */
+public class MainApplication extends Application {
+    // Width of the window
+    public static final int WINDOW_WIDTH = 1200;
+    // Height of the window
+    public static final int WINDOW_HEIGHT = 700;
+
+    /**
+     * Starts the application.
+     *
+     * @param stage the stage to run the application
+     */
+    @Override
+    public void start(Stage stage) {
+        GameController gameApp = new GameController();
+        Scene scene = new Scene(gameApp.getAppRoot(), WINDOW_WIDTH, WINDOW_HEIGHT);
+
+        stage.setTitle("Lowkey Time Travellers");
+        stage.setScene(scene);
+        stage.show();
+
+        gameApp.startGameLoop();
+    }
+
+    /**
+     * Launches the application.
+     *
+     * @param args not used
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
+
