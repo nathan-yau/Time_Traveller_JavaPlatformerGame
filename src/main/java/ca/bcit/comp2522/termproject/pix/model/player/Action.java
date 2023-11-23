46bcf86eb0de6a3895df0d9f6148be9b6150e662
NATHAN YAU <nshyau@gmail.com>
2023-11-22 16:41:46 -0800

    Add enumerations for player action in Action.java

---
+++ src/main/java/ca/bcit/comp2522/termproject/pix/model/player/Action.java
@@ -0,0 +1,13 @@
+package ca.bcit.comp2522.termproject.pix.model.player;
+
+/**
+ * Represents an action of the player.
+ * This enum is used to implement the action of the player.
+ *
+ * @author Nathan Yau
+ * @author Derek Woo
+ * @version 2023
+ */
+public enum Action {
+    IDLE, WALKING, MELEE_ATTACK, RANGE_ATTACK, JUMPING, HURTING, DYING
+}
