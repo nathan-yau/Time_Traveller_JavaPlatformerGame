1cfd74b0f6c343bdce84d9489b389f3163bb8c45
NATHAN YAU <nshyau@gmail.com>
2023-11-22 18:48:16 -0800

    Fix checkstyle warnings in PlayerType.java

--- src/main/java/ca/bcit/comp2522/termproject/pix/model/player/PlayerType.java
+++ src/main/java/ca/bcit/comp2522/termproject/pix/model/player/PlayerType.java
@@ -1,5 +1,12 @@
 package ca.bcit.comp2522.termproject.pix.model.player;
 
+/**
+ * Enumerates the types of player objects.
+ *
+ * @author Nathan Yau
+ * @author Derek Woo
+ * @version 2023-11
+ */
 public enum PlayerType {
     PLAYER
 }
