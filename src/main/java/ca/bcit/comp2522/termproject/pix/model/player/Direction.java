ae9145d993438ca52e551bc65fab36dd493bff25
NATHAN YAU <nshyau@gmail.com>
2023-11-22 16:41:59 -0800

    Add enumerations for player direction in Direction.java

---
+++ src/main/java/ca/bcit/comp2522/termproject/pix/model/player/Direction.java
@@ -0,0 +1,13 @@
+package ca.bcit.comp2522.termproject.pix.model.player;
+
+/**
+ * Represents the direction of the player.
+ * This enum is used to implement the direction of the player.
+ *
+ * @author Nathan Yau
+ * @author Derek Woo
+ * @version 2023
+ */
+public enum Direction {
+    FORWARD, BACKWARD
+}
