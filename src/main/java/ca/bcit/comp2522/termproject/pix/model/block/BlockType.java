eccfd0f1162b0bd8e9db990b0ad5475e1c697b8c
Derek Woo <me@derekw.co>
2023-11-22 19:25:42 -0800

    Add BlockType enumerations in BlockType.java

---
+++ src/main/java/ca/bcit/comp2522/termproject/pix/model/block/BlockType.java
@@ -0,0 +1,12 @@
+package ca.bcit.comp2522.termproject.pix.model.block;
+
+/**
+ * Enumerates the types of blocks.
+ *
+ * @author Nathan Yau
+ * @author Derek Woo
+ * @version 2023-11
+ */
+public enum BlockType {
+    SOLID_BLOCK, DECORATION_BLOCK, MOVING_BLOCK, MOVABLE_BLOCK, DISAPPEARING_BLOCK
+}
