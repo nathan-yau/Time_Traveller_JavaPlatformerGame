10ed8cfc0512c9b10e4858fd7228954b3920e89f
Derek Woo <me@derekw.co>
2023-11-22 19:28:42 -0800

    Enumerate block platform positions in PlatformPosition.java

---
+++ src/main/java/ca/bcit/comp2522/termproject/pix/model/platformgenerator/PlatformPosition.java
@@ -0,0 +1,15 @@
+package ca.bcit.comp2522.termproject.pix.model.platformgenerator;
+
+/**
+ * Enumerates the different positions on a platform.
+ *
+ * @author Nathan Yau
+ * @author Derek Woo
+ * @version 2023-11
+ */
+public enum PlatformPosition {
+    FLOOR, CEILING, FLOOR_LEFT_CORNER, FLOOR_RIGHT_CORNER, DIRT,
+    DIRT_RIGHT_CORNER, DIRT_LEFT_CORNER, LEFT_EDGE, RIGHT_EDGE, WALL,
+    WALL_BOTTOM, WALL_TOP, FLOOR_BOTTOM, FLOOR_BOTTOM_LEFT_CORNER, FLOOR_BOTTOM_RIGHT_CORNER,
+    DIRT_TURN_LEFT_CORNER, DIRT_TURN_UP_CORNER
+}
