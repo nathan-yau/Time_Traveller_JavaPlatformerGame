bd9eb771a3a831425342ed8e7ef2d77776e0060a
Derek Woo <me@derekw.co>
2023-11-24 16:14:09 -0800

    Add pickup item enumerations in PickUpItemType.java

---
+++ src/main/java/ca/bcit/comp2522/termproject/pix/model/pickupitem/PickUpItemType.java
@@ -0,0 +1,14 @@
+package ca.bcit.comp2522.termproject.pix.model.pickupitem;
+
+import ca.bcit.comp2522.termproject.pix.GameType;
+
+/**
+ * Enumerates the type of pickup items available.
+ *
+ * @author Nathan Yau
+ * @author Derek Woo
+ * @version 2023-11
+ */
+public enum PickUpItemType implements GameType {
+    HEALTH_POTION
+}
