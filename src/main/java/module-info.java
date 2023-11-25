24d0baa13cdcd1a95b7da61aa3d51116be345982
nathan-yau <nshyau@gmail.com>
2023-11-25 00:46:22 -0800

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
-module ca.bcit.comp2522.termproject {
+module ca.bcit.comp2522.termproject.pix {
     requires javafx.controls;
     requires javafx.fxml;
 
 
-    opens ca.bcit.comp2522.termproject to javafx.fxml;
-    exports ca.bcit.comp2522.termproject;
+    opens ca.bcit.comp2522.termproject.pix to javafx.fxml;
+    exports ca.bcit.comp2522.termproject.pix;
 }
