3a2d915ea1eb71eafe350ed06ee79378a2c2768d
Derek Woo <me@derekw.co>
2023-11-27 22:57:38 -0800

    Remove disappearance method from HealthPotion.java

--- src/main/java/ca/bcit/comp2522/termproject/pix/model/pickupitem/HealthPotion.java
+++ src/main/java/ca/bcit/comp2522/termproject/pix/model/pickupitem/HealthPotion.java
@@ -1,8 +1,6 @@
 package ca.bcit.comp2522.termproject.pix.model.pickupitem;
 
 import ca.bcit.comp2522.termproject.pix.model.ObjectType;
-import javafx.animation.PauseTransition;
-import javafx.util.Duration;
 
 /**
  * Represents a health potion.
@@ -26,30 +24,6 @@ public class HealthPotion extends PickUpItem {
     public HealthPotion(final int x, final int y, final int w, final int h,
                         final int currentLevel, final String imageName) {
         super(x, y, w, h, ObjectType.ITEM, PickUpItemType.HEALTH_POTION, currentLevel, imageName);
-    }
-
-    /**
-     * Disappear health potion when picked up.
-     *
-     * @return whether the health potion is picked up as a boolean
-     */
-    @Override
-    public boolean onPickUp() {
-        final int pauseDuration = 150;
-        PauseTransition pause = new PauseTransition(Duration.millis(pauseDuration));
-        pause.setOnFinished(event -> this.setVisible(false));
-        pause.play();
-        return true;
-    }
-
-    /**
-     * Use health potion.
-     *
-     * @return whether the health potion is used as a boolean
-     */
-    @Override
-    public boolean onUse() {
-        return false;
     }
 }
 
