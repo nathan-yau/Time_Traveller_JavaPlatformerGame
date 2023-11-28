dc8f2f45effb320c540ca899d11d32f30d8f379e
Derek Woo <me@derekw.co>
2023-11-27 23:01:29 -0800

    Add hitpoints and weapon array to Player.java

--- src/main/java/ca/bcit/comp2522/termproject/pix/model/player/Player.java
+++ src/main/java/ca/bcit/comp2522/termproject/pix/model/player/Player.java
@@ -9,6 +9,8 @@ import ca.bcit.comp2522.termproject.pix.model.Damageable;
 import ca.bcit.comp2522.termproject.pix.model.GameObject;
 import ca.bcit.comp2522.termproject.pix.model.Movable;
 import ca.bcit.comp2522.termproject.pix.model.ObjectType;
+import ca.bcit.comp2522.termproject.pix.model.weapon.Weapon;
+import ca.bcit.comp2522.termproject.pix.model.weapon.WeaponType;
 import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
@@ -26,8 +28,10 @@ import java.util.concurrent.CompletableFuture;
  * @version 2023
  */
 public final class Player extends GameObject<PlayerType> implements Combative, Damageable, Movable {
+    private static final int INITIAL_HEALTH_POINTS = 20;
     private static final double WALK_SPEED = 5;
     private static final double RUN_SPEED = 10;
+    private int healthPoint;
     private boolean jumpEnable = true;
     private boolean attackEnable = true;
     private double speed;
@@ -41,6 +45,7 @@ public final class Player extends GameObject<PlayerType> implements Combative, D
     private Timeline rangeAnimation;
     private Timeline walkAnimation;
     private Timeline jumpAnimation;
+    private final Weapon[] weaponArray = new Weapon[2];
 
     /**
      * Constructs a Player object.
@@ -50,6 +55,7 @@ public final class Player extends GameObject<PlayerType> implements Combative, D
      */
     public Player(final double x, final double y, final String imagePath) {
         super(x, y, ObjectType.PLAYER, PlayerType.PLAYER, imagePath);
+        this.healthPoint = INITIAL_HEALTH_POINTS;
         this.velocity = new Point2D(0, 0);
         this.direction = Direction.FORWARD;
         this.action = Action.IDLE;
@@ -267,10 +273,25 @@ public final class Player extends GameObject<PlayerType> implements Combative, D
     }
 
     /**
+     * Add a weapon to the Player's weapons array.
+     *
+     * @param weapon the weapon to add
+     */
+    public void addWeapon(final Weapon weapon) {
+        if (weapon.getWeaponType() == WeaponType.MELEE_WEAPON) {
+            this.weaponArray[0] = weapon;
+        } else {
+            this.weaponArray[1] = weapon;
+        }
+        System.out.println("Added: " + weapon);
+    }
+
+    /**
      * Increments the player's health potion counter by one.
      */
     public void incrementHealthPotionCounter() {
         this.healthPotionCounter++;
+        System.out.println("Health potion count: " + this.healthPotionCounter);
     }
 
     /**
@@ -278,6 +299,7 @@ public final class Player extends GameObject<PlayerType> implements Combative, D
      */
     public void decrementHealthPotionCounter() {
         this.healthPotionCounter--;
+        System.out.println("Health potion count: " + this.healthPotionCounter);
     }
 
     /**
@@ -293,6 +315,7 @@ public final class Player extends GameObject<PlayerType> implements Combative, D
      */
     public void incrementGoldCoinCounter() {
         this.goldCoinCounter++;
+        System.out.println("Gold Coin count: " + this.goldCoinCounter);
     }
 
     /**
@@ -391,5 +414,13 @@ public final class Player extends GameObject<PlayerType> implements Combative, D
         return null;
     }
 
-
+    /**
+     * Use a health potion.
+     */
+    public void useHealthPotion() {
+        if (healthPotionCounter > 0 && healthPoint < INITIAL_HEALTH_POINTS) {
+            this.healthPotionCounter--;
+            this.healthPoint++;
+        }
+    }
 }
