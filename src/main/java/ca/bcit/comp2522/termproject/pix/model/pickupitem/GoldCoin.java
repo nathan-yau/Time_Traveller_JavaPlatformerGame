a73c7f22993c3d9e794979718a2c71a630eb4b32
Derek Woo <me@derekw.co>
2023-11-27 22:57:30 -0800

    Remove disappearance method from GoldCoin.java

--- src/main/java/ca/bcit/comp2522/termproject/pix/model/pickupitem/GoldCoin.java
+++ src/main/java/ca/bcit/comp2522/termproject/pix/model/pickupitem/GoldCoin.java
@@ -1,8 +1,6 @@
 package ca.bcit.comp2522.termproject.pix.model.pickupitem;
 
 import ca.bcit.comp2522.termproject.pix.model.ObjectType;
-import javafx.animation.PauseTransition;
-import javafx.util.Duration;
 
 /**
  * Represents a gold coin pickup item.
@@ -25,29 +23,5 @@ public class GoldCoin extends PickUpItem {
     public GoldCoin(final int x, final int y, final int w, final int h,
                     final int currentLevel, final String imageName) {
         super(x, y, w, h, ObjectType.ITEM, PickUpItemType.GOLD_COIN, currentLevel, imageName);
-    }
-
-    /**
-     * Disappear gold coin when picked up.
-     *
-     * @return whether the gold coin is picked up as a boolean
-     */
-    @Override
-    public boolean onPickUp() {
-        final int pauseDuration = 150;
-        PauseTransition pause = new PauseTransition(Duration.millis(pauseDuration));
-        pause.setOnFinished(event -> this.setVisible(false));
-        pause.play();
-        return true;
-    }
-
-    /**
-     * Use the gold coin.
-     *
-     * @return whether the gold coin is used as a boolean
-     */
-    @Override
-    public boolean onUse() {
-        return false;
     }
 }
