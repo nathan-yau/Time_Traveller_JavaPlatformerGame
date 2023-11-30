67733977d3a1fba8bb8c57569e5b7e3bf741d1ab
NATHAN YAU <nshyau@gmail.com>
2023-11-30 10:37:18 -0800

    Fix check styles warnings for AnimatedObjects.java

--- src/main/java/ca/bcit/comp2522/termproject/pix/AnimatedObjects.java
+++ src/main/java/ca/bcit/comp2522/termproject/pix/AnimatedObjects.java
@@ -4,7 +4,18 @@ import javafx.animation.ParallelTransition;
 import javafx.animation.SequentialTransition;
 import javafx.animation.Timeline;
 
+/**
+ * Represents an object that can be animated.
+ *
+ * @author Nathan Yau
+ * @author Derek Woo
+ * @version 2023
+ */
 public interface AnimatedObjects {
+    /**
+     * Releases the timeline resources.
+     * @param timeline the timeline to be released
+     */
     static void releaseTimeline(Timeline timeline) {
         if (timeline != null) {
             timeline.stop();
@@ -12,6 +23,10 @@ public interface AnimatedObjects {
         }
     }
 
+    /**
+     * Releases the parallel transition resources.
+     * @param transition the parallel transition to be released
+     */
     static void releaseParallelTransition(ParallelTransition transition) {
         if (transition != null) {
             transition.stop();
@@ -19,6 +34,10 @@ public interface AnimatedObjects {
         }
     }
 
+    /**
+     * Releases the sequential transition resources.
+     * @param transition the sequential transition to be released
+     */
     static void releaseSequentialTransition(SequentialTransition transition) {
         if (transition != null) {
             transition.stop();
