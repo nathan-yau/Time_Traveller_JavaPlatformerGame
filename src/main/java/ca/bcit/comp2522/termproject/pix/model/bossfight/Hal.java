3ece61cbb691943d5871fc1969b7751048d3a61a
Derek Woo <me@derekw.co>
2023-12-02 12:15:56 -0800

    Update Hal y coordinate in Hal.java

--- src/main/java/ca/bcit/comp2522/termproject/pix/model/bossfight/Hal.java
+++ src/main/java/ca/bcit/comp2522/termproject/pix/model/bossfight/Hal.java
@@ -12,7 +12,7 @@ import ca.bcit.comp2522.termproject.pix.model.Enemy.EnemyType;
 public class Hal extends Boss {
     private static final int INITIAL_HEALTH_POINTS = 10;
     private static final int X_OFFSET = 780;
-    private static final int Y_OFFSET = 620;
+    private static final int Y_OFFSET = -150;
     private static final int WIDTH = 180;
     private static final int HEIGHT = 150;
     private static final int ATTACK_POINT = 10;
@@ -23,6 +23,6 @@ public class Hal extends Boss {
      * @param y the y coordinate of Hal as an int
      */
     public Hal(final int x, final int y) {
-        super(INITIAL_HEALTH_POINTS, X_OFFSET, Y_OFFSET, WIDTH, HEIGHT, EnemyType.HAL, ATTACK_POINT);
+        super(INITIAL_HEALTH_POINTS, X_OFFSET, y + Y_OFFSET, WIDTH, HEIGHT, EnemyType.HAL, ATTACK_POINT);
     }
 }
