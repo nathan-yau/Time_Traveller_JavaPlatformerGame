f228317f6e6e532a3bfef014a64cad4c8335afa5
Derek Woo <me@derekw.co>
2023-12-02 17:48:07 -0800

    Refine Hal coordinates in Hal.java

--- src/main/java/ca/bcit/comp2522/termproject/pix/model/bossfight/Hal.java
+++ src/main/java/ca/bcit/comp2522/termproject/pix/model/bossfight/Hal.java
@@ -11,11 +11,12 @@ import ca.bcit.comp2522.termproject.pix.model.Enemy.EnemyType;
  */
 public class Hal extends Boss {
     private static final int INITIAL_HEALTH_POINTS = 10;
-    private static final int X_OFFSET = 780;
-    private static final int Y_OFFSET = -150;
+    private static final int X_OFFSET = 750;
+    private static final int Y_OFFSET = -250;
     private static final int WIDTH = 180;
-    private static final int HEIGHT = 150;
-    private static final int ATTACK_POINT = 10;
+    private static final int HEIGHT = 180;
+    private static final int ATTACK_POINT = 15;
+
     /**
      * Constructs a Hal boss.
      *
@@ -23,6 +24,7 @@ public class Hal extends Boss {
      * @param y the y coordinate of Hal as an int
      */
     public Hal(final int x, final int y) {
-        super(INITIAL_HEALTH_POINTS, X_OFFSET, y + Y_OFFSET, WIDTH, HEIGHT, EnemyType.HAL, ATTACK_POINT);
+        super(INITIAL_HEALTH_POINTS, X_OFFSET - (WIDTH / 2), y + Y_OFFSET, WIDTH, HEIGHT,
+                EnemyType.HAL, ATTACK_POINT);
     }
 }
