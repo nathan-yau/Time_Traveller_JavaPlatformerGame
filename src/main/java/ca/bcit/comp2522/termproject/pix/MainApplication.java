34dbeb96ca99f34d3b8beb7bbf8c995beac8b8f2
Derek Woo <me@derekw.co>
2023-12-04 02:44:58 -0800

    Decompose main menu in MainApplication.java

--- src/main/java/ca/bcit/comp2522/termproject/pix/MainApplication.java
+++ src/main/java/ca/bcit/comp2522/termproject/pix/MainApplication.java
@@ -1,20 +1,11 @@
 package ca.bcit.comp2522.termproject.pix;
 
-import ca.bcit.comp2522.termproject.pix.gamecontroller.GameController;
-import ca.bcit.comp2522.termproject.pix.menu.MenuItem;
-import ca.bcit.comp2522.termproject.pix.menu.MenuTitle;
+import ca.bcit.comp2522.termproject.pix.screens.MenuScreen;
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Objects;
 
 /**
  * Represents the main application window.
@@ -30,86 +21,6 @@ public class MainApplication extends Application {
     /** Height of the window. */
     public static final int WINDOW_HEIGHT = 720;
 
-
-    /**
-     * Starts the Game.
-     *
-     * @param stage the stage to run the game in as a Stage
-     */
-    public void startGame(final Stage stage) {
-        GameController gameApp = new GameController(WINDOW_WIDTH, WINDOW_HEIGHT);
-        Scene scene = new Scene(gameApp.getAppRoot(), WINDOW_WIDTH, WINDOW_HEIGHT);
-
-        stage.setScene(scene);
-        stage.show();
-
-        gameApp.insertKeyboardListeners();
-        gameApp.startGameLoop();
-    }
-
-    private final class Menu {
-        private final Pane root = new Pane();
-        private final ArrayList<String> menuItems = new ArrayList<>();
-        private final VBox menuBox = new VBox(-5);
-
-        private Menu(final String[] menuItems) {
-            this.menuItems.addAll(Arrays.asList(menuItems));
-        }
-
-        /**
-         * Starts the Menu.
-         */
-        private void addMenu() {
-            final int menuItemTopPadding = 20;
-
-            menuBox.setTranslateX(75);
-            menuBox.setTranslateY(505);
-            this.menuItems.forEach(data -> {
-                MenuItem item;
-                item = new MenuItem(data);
-                item.setOnMouseClicked(event -> {
-                    switch (data) {
-                        case "New Game" -> startGame((Stage) root.getScene().getWindow());
-                        case "Load" -> System.out.println("Load game");
-                        case "Exit" -> Platform.exit();
-                        default -> throw new IllegalStateException("Unexpected value: " + data);
-                    }
-                });
-                item.setPadding(new javafx.geometry.Insets(menuItemTopPadding, 0, 0, 0));
-                menuBox.getChildren().addAll(item);
-            });
-
-            root.getChildren().add(menuBox);
-        }
-
-        /*
-         * Add the title of the game.
-         */
-        private void addMenuTitle() {
-            final int maxMenuTitleWidth = 390;
-            final int titleXOffset = 75;
-            final int titleYOffset = 410;
-            MenuTitle title = new MenuTitle("Lowkey Time Travellers", maxMenuTitleWidth);
-
-            title.setTranslateX(titleXOffset);
-            title.setTranslateY(titleYOffset);
-
-            root.getChildren().add(title);
-        }
-
-        /*
-         * Adds the menu background.
-         */
-        private void addMenuBackground() {
-            ImageView bgImg = new ImageView(new Image(
-                    Objects.requireNonNull(getClass().getResourceAsStream(String.format("menu/%s", "menuBg.gif")))
-                ));
-            bgImg.setFitHeight(WINDOW_HEIGHT);
-            bgImg.setFitWidth(WINDOW_WIDTH);
-            root.getChildren().add(bgImg);
-        }
-    }
-
     /**
      * Starts the application.
      *
@@ -117,17 +28,22 @@ public class MainApplication extends Application {
      */
     @Override
     public void start(final Stage stage) {
-        Menu menu = new Menu(new String[]{"New Game", "Load", "Exit"});
-        Scene scene = new Scene(menu.root, WINDOW_WIDTH, WINDOW_HEIGHT);
+        final int maxMenuTitleWidth = 390;
+        final int titleXOffset = 75;
+        final int titleYOffset = 410;
+
+        MenuScreen menuScreen = new MenuScreen(WINDOW_WIDTH, WINDOW_HEIGHT);
+        Scene scene = new Scene(menuScreen.getRoot(), WINDOW_WIDTH, WINDOW_HEIGHT);
 
         stage.setTitle("Lowkey Time Travellers");
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
 
-        menu.addMenuBackground();
-        menu.addMenuTitle();
-        menu.addMenu();
+        menuScreen.addBackground("menuBg.gif");
+        menuScreen.addTitle("Lowkey Time Travellers", Color.WHITE, TextAlignment.LEFT,
+                maxMenuTitleWidth, titleXOffset, titleYOffset);
+        menuScreen.addMenu();
     }
 
     /**
