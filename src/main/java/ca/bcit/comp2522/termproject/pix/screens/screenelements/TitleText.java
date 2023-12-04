13bd30ce28dc021139dd92d79dad1c1d298b694a
Derek Woo <me@derekw.co>
2023-12-04 12:34:08 -0800

    Refactor TitleText.java to extend TextElement

--- src/main/java/ca/bcit/comp2522/termproject/pix/screens/screenelements/TitleText.java
+++ src/main/java/ca/bcit/comp2522/termproject/pix/screens/screenelements/TitleText.java
@@ -2,7 +2,6 @@ package ca.bcit.comp2522.termproject.pix.screens.screenelements;
 
 import ca.bcit.comp2522.termproject.pix.DefaultFont;
 import javafx.scene.effect.DropShadow;
-import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
@@ -15,32 +14,44 @@ import javafx.scene.text.TextAlignment;
  * @author Derek Woo
  * @version 2023-12
  */
-public class TitleText extends Pane {
+public class TitleText extends TextElement {
+
     /**
      * Constructs a TitleText.
      *
-     * @param title the title as a String
+     * @param content the title as a String
      * @param color the color of the title as a Color
      * @param alignment the alignment of the title as a TextAlignment
      * @param maxWidth the maximum width of the title as a double
      */
-    public TitleText(final String title, final Color color, final TextAlignment alignment, final double maxWidth) {
+    public TitleText(final String content, final Color color, final TextAlignment alignment, final double maxWidth) {
+        super(content, color, alignment, maxWidth);
+    }
+
+    /**
+     * Formats the text.
+     *
+     * @param spreadContent the spread content of the text to be formatted as a String
+     * @param color the color of the text as a Color
+     * @param alignment the alignment of the text as a TextAlignment
+     * @param maxWidth the maximum width of the text as a double
+     * @return the formatted text as a Text
+     */
+    @Override
+    protected Text formatText(final String spreadContent, final Color color,
+                                       final TextAlignment alignment, final double maxWidth) {
         final int shadowRadius = 40;
         Font font = DefaultFont.getDefaultTitleFont();
+        Text text = new Text(spreadContent);
 
-        StringBuilder spread = new StringBuilder();
-        for (char c : title.toCharArray()) {
-            spread.append(c).append(" ");
-        }
-
-        Text text = new Text(spread.toString());
         text.setFont(font);
         text.setFill(color);
         text.setTextAlignment(alignment);
         text.setEffect(new DropShadow(shadowRadius, Color.BLACK));
         text.setWrappingWidth(maxWidth);
 
-        getChildren().addAll(text);
+        return text;
     }
 }
+
 
