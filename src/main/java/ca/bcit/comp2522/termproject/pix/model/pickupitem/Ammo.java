6f66e8daf84c521986a114257c17f2153c3daabf
NATHAN YAU <nshyau@gmail.com>
2023-12-08 18:48:47 -0800

    Fix check styles warnings

--- src/main/java/ca/bcit/comp2522/termproject/pix/model/pickupitem/Ammo.java
+++ src/main/java/ca/bcit/comp2522/termproject/pix/model/pickupitem/Ammo.java
@@ -1,15 +1,22 @@
 package ca.bcit.comp2522.termproject.pix.model.pickupitem;
 
+/**
+ * Represents an Ammo object.
+ *
+ * @author Nathan Yau
+ * @author Derek Woo
+ * @version 2023-11
+ */
 public class Ammo extends PickUpItem {
     /**
-     * Constructs a PickUpItem.
+     * Constructs an Ammo object.
      *
-     * @param x              the x coordinate of the PickUpItem as an int
-     * @param y              the y coordinate of the PickUpItem as an int
-     * @param w              the width of the PickUpItem as an int
-     * @param h              the height of the PickUpItem as an int
+     * @param x the x coordinate of the Ammo
+     * @param y the y coordinate of the Ammo
+     * @param w the width of the Ammo
+     * @param h the height of the Ammo
      */
-    public Ammo(int x, int y, int w, int h) {
+    public Ammo(final int x, final int y, final int w, final int h) {
         super(x, y, w, h, PickUpItemType.AMMO);
     }
 }
