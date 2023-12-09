01ecb367d8a6437083dbad07a612c3cee90ab06b
NATHAN YAU <nshyau@gmail.com>
2023-12-08 18:48:50 -0800

    Fix check styles warnings

--- src/main/java/ca/bcit/comp2522/termproject/pix/model/pickupitem/BossEvent.java
+++ src/main/java/ca/bcit/comp2522/termproject/pix/model/pickupitem/BossEvent.java
@@ -1,15 +1,22 @@
 package ca.bcit.comp2522.termproject.pix.model.pickupitem;
 
+/**
+ * Represents a BossEvent object.
+ *
+ * @author Nathan Yau
+ * @author Derek Woo
+ * @version 2023-11
+ */
 public class BossEvent extends PickUpItem {
     /**
-     * Constructs a PickUpItem.
+     * Constructs a BossEvent object.
      *
-     * @param x              the x coordinate of the PickUpItem as an int
-     * @param y              the y coordinate of the PickUpItem as an int
-     * @param w              the width of the PickUpItem as an int
-     * @param h              the height of the PickUpItem as an int
+     * @param x the x coordinate of the BossEvent
+     * @param y the y coordinate of the BossEvent
+     * @param w the width of the BossEvent
+     * @param h the height of the BossEvent
      */
-    public BossEvent(int x, int y, int w, int h) {
+    public BossEvent(final int x, final int y, final int w, final int h) {
         super(x, y, w, h, PickUpItemType.BOSS_TRIGGER);
     }
 
